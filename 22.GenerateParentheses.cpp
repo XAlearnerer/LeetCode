fba5723dcecc7f979f3b76f0deacf76e7e01cb1c
XAlearnerer <43571828+XAlearnerer@users.noreply.github.com>
2018-10-20 17:02:12 +0800

    2018/10/20 problem20

--- 22.GenerateParentheses.cpp
+++ 22.GenerateParentheses.cpp
@@ -2,15 +2,54 @@
 using namespace std;
 #include<vector>
 #include<stack>
+#include<set>
+
+//class Solution {
+//public:
+//	vector<string> generateParenthesis(int n) {
+//		set<string> t;
+//		if (n == 0) t.insert("");
+//		else {
+//			vector<string> pre = generateParenthesis(n - 1);
+//			for (auto a : pre) {
+//				for (int i = 0; i < a.size(); ++i) {
+//					if (a[i] == '(') {
+//						a.insert(a.begin() + i + 1, '(');
+//						a.insert(a.begin() + i + 2, ')');
+//						t.insert(a);
+//						a.erase(a.begin() + i + 1, a.begin() + i + 3);
+//					}
+//				}
+//				t.insert("()" + a);
+//			}
+//		}
+//		return vector<string>(t.begin(), t.end());
+//	}
+//};
+
+
 
 class Solution {
 public:
 	vector<string> generateParenthesis(int n) {
-		stack<int> s;
-		int i = 0, j = 0;
-
-
+		vector<string> res;
+		generateParenthesisDFS(n, n, "", res);
+		return res;
+	}
+	void generateParenthesisDFS(int left, int right, string out, vector<string> &res) {
+		if (left > right) return;
+		if (left == 0 && right == 0) res.push_back(out);
+		else {
+			if (left > 0) generateParenthesisDFS(left - 1, right, out + '(', res);
+			if (right > 0) generateParenthesisDFS(left, right - 1, out + ')', res);
+		}
 	}
 };
+
+
+
+
+
+
 
 
