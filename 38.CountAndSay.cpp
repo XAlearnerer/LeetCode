9dc5a0669e232bbfb42938f9da728552ca963a80
zhao <breezezjt@gmail.com>
2018-10-23 21:57:41 +0800

    2018/10/23

--- 38.CountAndSay.cpp
+++ 38.CountAndSay.cpp
@@ -6,7 +6,7 @@ using namespace std;
 #include <string>
 
 
-class Solution {
+class Solution2 {
 public:
     string countAndSay(int n) {
         if(n<1 || n>30)
@@ -42,6 +42,59 @@ public:
         ++count;
         return result;
 
+    }
+};
+
+
+class Solution3 {
+public:
+    string countAndSay(int n) {
+        if(n<1 || n>30)
+            return "";
+        if(n==1) return "1";
+        string s=countAndSay(n-1);
+
+        string result="";
+        int i=0,j=1;
+        while(j!=s.length())
+        {
+            if(s[i]==s[j])
+                ++j;
+            else
+            {
+                result=result+to_string(j-i)+s[i];
+                i=j;
+            }
+        }
+        result=result+to_string(j-i)+s[i];
+        return result;
+    }
+};
+
+class Solution {
+public:
+    string countAndSay(int n) {
+        if (n < 1) return "";
+
+        string s = "1";
+
+        for (int k = 1; k< n; ++k) {
+            string result="";
+            int i=0,j=1;
+            while(j!=s.length())
+            {
+                if(s[i]==s[j])
+                    ++j;
+                else
+                {
+                    result=result+to_string(j-i)+s[i];
+                    i=j;
+                }
+            }
+            result=result+to_string(j-i)+s[i];
+            s = result;
+        }
+        return s;
     }
 };
 
