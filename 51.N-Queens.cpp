2fb1f8691a3bf6881557b3b8d886de690a00da11
XAlearnerer <43571828+XAlearnerer@users.noreply.github.com>
2018-11-03 16:31:42 +0800

    2018/11/03

---
+++ 51.N-Queens.cpp
@@ -0,0 +1,65 @@
+#include<iostream>
+using namespace std;
+#include<vector>
+#include<math.h>
+#include<string>
+
+class Solution {
+public:
+	vector<vector<string>> solveNQueens(int n) {
+		vector<int> solve(n, -1);
+		vector<vector<string>> res;
+		Nqueen(solve, 0, n, res);
+		return res;
+
+	}
+
+	void Nqueen(vector<int>& solve, int k, int n, vector<vector<string>>& res)
+	{
+		if (k == n)
+		{
+			string s(n, '.');
+			vector<string> vs(n, s);
+			for (int i = 0; i != n; ++i)
+			{
+				vs[i][solve[i]] = 'Q';
+			}
+			res.push_back(vs);
+			
+			for (auto j : solve)
+				cout << j << " ";
+			cout << endl;
+
+			return;
+		}
+		for (int i = 0; i != n; ++i)
+		{
+			solve[k] = i;
+			if (check(solve, k, n))
+				Nqueen(solve, k + 1, n, res);
+		}
+	}
+
+	bool check(vector<int>& solve, int k, int n)
+	{
+		for (int i = 0; i != k; ++i)
+		{
+			if (solve[k] == solve[i]) return false;
+			if (abs(k - i) == abs(solve[k] - solve[i])) return false;
+		}
+
+		return true;
+	}
+};
+
+int main()
+{
+	Solution n;
+	vector<vector<string> >vv = n.solveNQueens(4);
+	for (auto i : vv) {
+		for (auto j : i)
+			cout << j << " ";
+		cout << endl;
+	}
+	return 0;
+}
--- 54.SpiralMatrix_旋转输出矩阵.cpp
+++ 54.SpiralMatrix_旋转输出矩阵.cpp
@@ -2,7 +2,7 @@
 #include<vector>
 using namespace std;
 
-class Solution {
+class Solution { // 这个函数不知道怎么救了
 public:
 	vector<int> spiralOrder(vector<vector<int>>& matrix) {
 		if (matrix.empty() || matrix[0].empty()) return {};
@@ -19,18 +19,24 @@ public:
 			{
 				v.push_back(matrix[m - 1 - turnrow][i]);
 			}
+			//v.push_back(99);
+
 			int j = m - turnrow;
 			for (; j <= turnrow; ++j)
 			{
 				v.push_back(matrix[j][turncol]);
 			}
+			//v.push_back(99);
+
 			int k = turncol - 1;
-			for (; k > n - 1 - turncol; --k)
+			for (; k >= n - 1 - turncol && turnrow >= 1; --k)
 			{
 				v.push_back(matrix[turnrow][k]);
 			}
-			int l = turnrow;
-			for (; l > m - 1 - turnrow; --l)
+			//v.push_back(99);
+
+			int l = turnrow - 1;
+			for (; l > m - 1 - turnrow && turncol >= 1; --l)
 			{
 				v.push_back(matrix[l][n - 1 - turncol]);
 			}
@@ -42,12 +48,37 @@ public:
 	}
 };
 
+class Solution2 {
+public:
+	vector<int> spiralOrder(vector<vector<int>>& matrix) {
+		if (matrix.empty()) return {};
+		int m = matrix.size(), n = matrix[0].size();
+		vector<int> spiral(m * n);
+		int u = 0, d = m - 1, l = 0, r = n - 1, k = 0;
+		while (true) {
+			// up
+			for (int col = l; col <= r; col++) spiral[k++] = matrix[u][col];
+			if (++u > d) break;
+			// right
+			for (int row = u; row <= d; row++) spiral[k++] = matrix[row][r];
+			if (--r < l) break;
+			// down
+			for (int col = r; col >= l; col--) spiral[k++] = matrix[d][col];
+			if (--d < u) break;
+			// left
+			for (int row = d; row >= u; row--) spiral[k++] = matrix[row][l];
+			if (++l > r) break;
+		}
+		return spiral;
+	}
+};
 
-int main()
+int main1601()
 {
-	//vector<vector<int>> vv = { {1, 2, 3, 4},{5, 6, 7, 8},{9,10,11,12} };
+	vector<vector<int>> vv = { {1, 2, 3, 4},{5, 6, 7, 8},{9,10,11,12} };
 	//vector<vector<int>> vv = { { 1} };
-	vector<vector<int>> vv = { { 3} ,{2} };
+	//vector<vector<int>> vv = { {3} ,{2} };
+	//vector<vector<int>> vv = { { 1,2 } ,{ 3,4 } };
 	Solution n;
 	vector<int > v = n.spiralOrder(vv);
 	for (auto i : v)
@@ -55,3 +86,5 @@ int main()
 	cout << endl;
 	return 0;
 }
+
+
