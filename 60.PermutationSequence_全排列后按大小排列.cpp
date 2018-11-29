282ea66a3ac1c3852324186a789dd9cb1093068a
zhao <breezezjt@gmail.com>
2018-11-29 22:13:00 +0800

    2018/11/29

--- 60.PermutationSequence_全排列后按大小排列.cpp
+++ 60.PermutationSequence_全排列后按大小排列.cpp
@@ -3,7 +3,8 @@ using namespace std;
 #include<vector>
 #include<string>
 
-class Solution {
+
+class Solution2 {
 public:
 	string getPermutation(int n, int k) {
 		int ki = 1;
@@ -13,7 +14,6 @@ public:
 		string ss = " ";
 		getK(solu, 0, ki, k, ss);
 		return ss;
-
 	}
 	void getK(string& solu, int begin, int& ki, int k,string& ss)
 	{
@@ -33,13 +33,42 @@ public:
 			getK(solu, begin + 1, ki, k,ss);
 			swap(solu[begin], solu[i]);
 		}
-	
 	}
 };
 
-int main1807()
+
+class Solution {
+public:
+    string getPermutation(int n, int k) {
+        vector<int> factorial(n,1);
+        string nums(n,'a');
+        string res="";
+
+        for(int i=1;i!=n;++i)
+            factorial[i]=factorial[i-1]*i;
+        for(int i=0;i!=n;++i)
+            nums[i]=i+1+'0';
+        int a;
+        --k;
+
+        while(n>=1)
+        {
+            a=k/factorial[n-1];
+            res+=nums[a];
+            k%=factorial[n-1];
+            nums.erase(a,1);
+            --n;
+        }
+
+        return res;
+    }
+};
+
+
+
+int main2205()
 {
 	Solution n;
-	cout << n.getPermutation(3, 3) << endl;
+	cout << n.getPermutation(4, 17) << endl;
 	return 0;
 }
