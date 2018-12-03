ef961b934954e0c55cf074e66116d93fd47bfff0
zhao <breezezjt@gmail.com>
2018-12-03 15:25:17 +0800

    2018/12/03

---
+++ Array/1.TwoSum.cpp
@@ -0,0 +1,35 @@
+#include<iostream>
+#include<vector>
+using namespace std;
+#include<map>
+
+
+class Solution {
+public:
+	vector<int> twoSum(vector<int>& nums, int target) {
+		map<int, int> m;
+		for (int i = 0; i < nums.size(); ++i)
+		{
+			if (m.count(target - nums[i]))
+			{
+				return { i, m[target - nums[i]] };
+			}
+			m[nums[i]] = i;
+		}
+		return {};
+	}
+};
+
+
+//int main()
+//{
+//	vector<int> v = { 2, 7, 11, 15 };
+//	int target = 9;
+//	Solution n;
+//
+//	vector<int> v2 = n.twoSum(v, target);
+//
+//	for (auto i : v2)
+//		cout << i << " ";
+//	return 0;
+//}
