2a438be7b86959bd9a59352286c66d0ee7bb9d0d
zhao <breezezjt@gmail.com>
2018-12-23 17:08:52 +0800

    2018/12/23

---
+++ Tree/116.PopulatingNextRightPointsInEachPoint_二叉树中每一个点设置向右的指针.cpp
@@ -0,0 +1,24 @@
+//
+// Created by zhao on 18-12-23.
+//
+
+#include <iostream>
+using namespace std;
+
+struct TreeLinkNode {
+  int val;
+  TreeLinkNode *left, *right, *next;
+  TreeLinkNode(int x) : val(x), left(NULL), right(NULL), next(NULL) {}
+};
+
+class Solution {
+public:
+    void connect(TreeLinkNode *root) {
+        if (!root) return;
+        if (root->left) root->left->next = root->right;
+        if (root->right) root->right->next = root->next? root->next->left : NULL;
+        connect(root->left);
+        connect(root->right);
+    }
+};
+
