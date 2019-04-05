6cefd5f5c8ed6106fb535ebdf9d132bbf9481dbd
zhao <breezezjt@gmail.com>
2019-04-05 22:13:29 +0800

    20190405

--- String/459.RepeatedSubstringPattern_判断是否为子串重复.cpp
+++ String/459.RepeatedSubstringPattern_判断是否为子串重复.cpp
@@ -3,6 +3,30 @@
 //
 
 #include <iostream>
+#include<string>
 using namespace std;
+
+
+class Solution {
+public:
+    bool repeatedSubstringPattern(string s) {
+		for (int i = 1; i <= s.length() / 2; ++i)
+		{
+			if (s.length() % i == 0)
+			{
+				int sublen = s.length() / i;
+				string ts = "";
+				for (int j = 0; j < sublen; ++j)
+				{
+					ts += s.substr(0, i);
+				}
+				if (s == ts) return true;
+			}
+		}
+		return false;
+    }
+};
+
+
 
 
