20ae718d5c92603cfc5cba5d9926ca7dad27afa9
XAlearnerer <43571828+XAlearnerer@users.noreply.github.com>
2019-04-21 22:04:27 +0800

    20190421

--- TwoPointer/209.minimumSizeSubarraySize_和为给定值的长度最小的相邻子数组.cpp
+++ TwoPointer/209.minimumSizeSubarraySize_和为给定值的长度最小的相邻子数组.cpp
@@ -1,12 +1,32 @@
 #include<iostream>
 #include<vector>
+#include<algorithm>
 
 using namespace std;
-
 
 class Solution {
 public:
 	int minSubArrayLen(int s, vector<int>& nums) {
+		int i = 0, j = 0;
+		int len = nums.size() + 1;
+		int sum = 0;
+		while (j != nums.size())
+		{
+			while (j != nums.size() && sum < s)
+			{
+				sum += nums[j];
+				++j;
+			}
+			while (sum >= s)
+			{
+				len = min(len, j - i);
+				sum -= nums[i];
+				++i;
+			}
+		}
+		if (len == nums.size() + 1) return 0;
+		else
+			return len;
 
 	}
 };
