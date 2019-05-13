9a3a8ca933e9f57513e30da2692492036b25bde5
zhao <breezezjt@gmail.com>
2019-05-13 16:03:36 +0800

    20190513

--- BinarySearch/719.FindK-thSmallestPairDistance_找到第k小的距离对.cpp
+++ BinarySearch/719.FindK-thSmallestPairDistance_找到第k小的距离对.cpp
@@ -8,6 +8,32 @@
 //Kth Smallest Number in Multiplication Table
 //K-th Smallest Prime Fraction
 
+#include <iostream>
+#include <vector>
+#include <algorithm>
+
+using namespace std;
 
 
+class Solution {
+public:
+    int smallestDistancePair(vector<int>& nums, int k) {
+        sort(nums.begin(),nums.end());
+        int n=nums.size();
+        int left=0,right=nums.back()-nums[0];
 
+        while (left < right) {
+            int mid = (right + left) / 2, cnt = 0, start = 0;
+            for(int i=0;i<nums.size();++i)
+            {
+                while (start < n && nums[i] - nums[start] > mid) ++start;
+                cnt += i - start;
+            }
+
+            if (cnt < k) left = mid + 1;
+            else right = mid;
+        }
+
+        return right;
+    }
+};
