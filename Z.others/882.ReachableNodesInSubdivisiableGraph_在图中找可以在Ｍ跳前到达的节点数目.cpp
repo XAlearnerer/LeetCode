03c14c2ade24f6d05d1bd239925b1dc11ddce1f3
zhao <breezezjt@gmail.com>
2019-05-23 21:40:29 +0800

    20190523

--- Z.others/882.ReachableNodesInSubdivisiableGraph_在图中找可以在Ｍ跳前到达的节点数目.cpp
+++ Z.others/882.ReachableNodesInSubdivisiableGraph_在图中找可以在Ｍ跳前到达的节点数目.cpp
@@ -8,7 +8,11 @@
 
 using namespace std;
 
-class Solution {
+
+/////　 Dijkstra + Priority Queue　/////
+
+
+class Solution_Lee215 {
 public:
     int reachableNodes(vector<vector<int>> edges, int M, int N) {
         unordered_map<int, unordered_map<int, int>> e;
@@ -39,13 +43,96 @@ public:
 };
 
 
-int main()
-{
-    vector<vector<int>> edges ={{0,1,10},{0,2,1},{1,2,2}};
-    int M=6, N=3;
-    Solution n;
-    cout<<n.reachableNodes(edges,M,N)<<endl;
 
-    return 0;
-}
+class Solution {
+public:
+    struct node
+    {
+        int src;
+        int move;
+        node(int s,int m):src(s),move(m){}
+    };
 
+    struct MyCmp {
+    public:
+        bool operator() (const node& l, const node& r) {
+            return l.move > r.move;
+        }
+    };
+
+    int reachableNodes(vector<vector<int>>& edges, int M, int N) {
+        unordered_map<int,unordered_map<int,int>> graph;
+        unordered_map<int,int> visited;
+
+//        auto cmp=[](node a,node b){ return a.move>b.move;};
+//        priority_queue<node,vector<node>,decltype(cmp)> q;
+        priority_queue<node,vector<node>,MyCmp> q;
+
+        for(auto i:edges)
+        {
+            graph[i[0]][i[1]]=i[2];
+            graph[i[1]][i[0]]=i[2];
+        }
+
+        int res=0;
+        node n(0,0);
+        q.push(n);
+
+        while(!q.empty())
+        {
+            node cur=q.top();
+            q.pop();
+            int src=cur.src;
+            int move=cur.move;
+            if(move>M) break;
+
+            if(visited[src]) continue;
+            visited[src]=1;
+            ++res;
+
+            for(auto &it:graph[src])
+            {
+                int dst=it.first;
+                int weight=it.second;
+
+                if(visited[dst]) {
+                    res += min(M - move, graph[src][dst]);
+                }
+                else
+                {
+                    int nextweight=move+weight+1;
+                    if(nextweight>M)
+                    {
+                        res+=M-move;
+                        //graph[dst][src]-=weight-M;
+                        graph[dst][src] -= M-move;
+                    }
+                    else
+                    {
+                        //graph[src][dst]=0;
+                        graph[dst][src]=0;
+                        res+=weight;
+                        node nextnode(dst,nextweight);
+                        q.push(nextnode);
+                    }
+                }
+            }
+        }
+
+
+        return res;
+    }
+};
+
+
+//
+//int main()
+//{
+//    vector<vector<int>> edges = {{0,2,3},{0,4,4},{2,3,8},{1,3,5},{0,3,9},{3,4,6},{0,1,5},{2,4,6},{1,2,3},{1,4,1}};
+//    int M=8, N=5;
+//    Solution n;
+//    cout<<n.reachableNodes(edges,M,N)<<endl;
+//
+//    return 0;
+//}
+//
