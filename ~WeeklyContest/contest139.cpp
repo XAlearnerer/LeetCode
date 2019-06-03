d355bad6c7b5cd37bfc4fc8168dc58486e6e5ac3
XAlearnerer <43571828+XAlearnerer@users.noreply.github.com>
2019-06-03 23:03:45 +0800

    20190603

--- ~WeeklyContest/contest139.cpp
+++ ~WeeklyContest/contest139.cpp
@@ -12,20 +12,33 @@
 
 using namespace std;
 
-class Solution {
+class Solution1071 {
 public:
 	string gcdOfStrings(string str1, string str2) {
 		if (str1.empty() || str2.empty()) return "";
-
-		string s, l;
-		if (str1.size() < str2.size()) { s = str1; l = str2; }
-		else { s = str2; l = str1; }
-
-		if (search(l.begin(), l.end(), s.begin(), s.end()) == l.end()) return "";
-		else
+		int res = 0;
+		for (int i = 1; i <= min(str1.size(),str2.size()); ++i)
 		{
-
+			if (str1.size() % i == 0 && str2.size() % i == 0)
+			{
+				string cur = str1.substr(0, i);
+				string a, b;
+				for (int p = 0; p < str1.size() / i; ++p) a += cur;
+				for (int q = 0; q < str2.size() / i; ++q) b += cur;
+				if (a == str1 && b == str2) res = i;
+			}
 		}
+		return str1.substr(0, res);
+
+		//string s, l;
+		//if (str1.size() < str2.size()) { s = str1; l = str2; }
+		//else { s = str2; l = str1; }
+
+		//if (search(l.begin(), l.end(), s.begin(), s.end()) == l.end()) return "";
+		//else
+		//{
+
+		//}
 
 	//	vector<int> v(26, 0);
 	//	for (auto i : s) ++v[i - 'A'];
@@ -48,20 +61,47 @@ public:
 	//		}
 	//	}
 
-
 	}
 };
 
-int main()
-{
-	string s1 = "ABCABC";
-	string s2 = "ABCABCABC";
-	Solution n;
-	cout << n.gcdOfStrings(s1, s2) << endl;
-	return 0;
-}
+
+//////////////////////////////////////////////////////////////////////////
 
 
+//  e.g.1
+//  000     110
+//  001 ->  111 <-all ones
+//	110     000 <-all zeros
+//	       ^^
+//	       flip columns
+//
+//	e.g.2
+//	00111       11111 <-all ones
+//	11000       00000 <-all zeros
+//	10101  ->   01101
+//	10100       01100
+//	00111       11111 <-all ones
+//			   ^^
+//			   flip columns
+//
+//	now we look closer, actually row0, row1 and row4 have the same pattern originally
+//	row0 = 00111
+//	row1 = 11000
+//	row4 = 00111
+//
+//	so we can just flip the rows count the pattern occurrence 
+//  instead of brainlessly flipping the columns
+//
+
+
+class Solution {
+public:
+	int maxEqualRowsAfterFlips(vector<vector<int>>& matrix) {
+
+
+
+	}
+};
 
 
 
