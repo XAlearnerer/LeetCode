7b07f44fae116696a17251df5b18376d37e3918d
XAlearnerer <43571828+XAlearnerer@users.noreply.github.com>
2019-07-15 17:11:30 +0800

    20190715

---
+++ ~Top 100 Liked Questions/301.删除无效的括号.cpp
@@ -0,0 +1,74 @@
+﻿#include<iostream>
+#include<vector>
+#include<unordered_set>
+#include<queue>
+
+using namespace std;
+
+
+class Solution {
+public:
+	vector<string> removeInvalidParentheses(string s) {
+		vector<string> res;
+		unordered_set<string> t;
+		queue<string> q;
+		q.push(s);
+
+		bool found = false;
+
+		while (!q.empty())
+		{
+			string cur = q.front();
+			q.pop();
+			if (helper(cur))
+			{
+				res.push_back(cur);
+				//continue;
+				found = true;
+			}
+
+			if (found) continue; //? 题目要求 寻找最修改最少的 字符串，已存在 成功的字符串，只需查找与其长度相同的字符串即可 
+
+			for (int i = 0; i < cur.size(); ++i)
+			{
+				if (cur[i] != '(' && cur[i] != ')') continue;
+				string temp = cur.substr(0, i) + cur.substr(i + 1);
+				if (t.count(temp) == 0)
+				{
+					t.insert(temp);
+					q.push(temp);
+				}
+			}
+		}
+
+		return res;
+	}
+
+
+	bool helper(string s)
+	{
+		int count = 0;
+		for (auto i : s)
+		{
+			if (i == '(') ++count;
+			if (i == ')')
+			{
+				--count;
+				if (count < 0) return false;
+			}
+		}
+		if (count == 0) return true;
+		else return false;
+	}
+
+};
+
+
+
+
+
+
+
+
+
+
